56c123f230c3634287fa8baac7ecf88b879396cc
Zaft <kurtfitz@hotmail.com>
2023-02-16 11:02:42 -0800

    complete execise 3-20, add eslint and config

--- backend/index.js
+++ backend/index.js
@@ -8,173 +8,147 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 // app.use(unknownEndpoint)
+// eslint-disable-next-line no-unused-vars
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 // morgan('tiny')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-const mongoose = require('mongoose')
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck (be)", 
-      "number": "39-23-6423122"
-    }
-]
-
-const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3
-    }
-})
+// let persons = [
+//     {
+//       "id": 1,
+//       "name": "Arto Hellas",
+//       "number": "040-123456"
+//     },
+//     {
+//       "id": 2,
+//       "name": "Ada Lovelace",
+//       "number": "39-44-5323523"
+//     },
+//     {
+//       "id": 3,
+//       "name": "Dan Abramov",
+//       "number": "12-43-234345"
+//     },
+//     {
+//       "id": 4,
+//       "name": "Mary Poppendieck (be)",
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons/', (request, response) => {
-    // response.json(persons)
-    console.log("getAll")
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  // response.json(persons)
+  console.log('getAll')
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    console.log('delete id', id)
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = Number(request.params.id)
+  console.log('delete id', id)
+  Person.findByIdAndDelete(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    // const id = mongoose.Types.ObjectId(request.params.id)
-    const id = request.params.id
-    console.log("get person by id ", id)
-    Person.findById(id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  // const id = mongoose.Types.ObjectId(request.params.id)
+  const id = request.params.id
+  console.log('get person by id ', id)
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    console.log("put body", request.body)
-    console.log("put params", request.params)
+  console.log('put body', request.body)
+  console.log('put params', request.params)
 
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { name, number}, 
-        { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    // console.log("request headers", request.headers)
-    // console.log("request body", request.body)
-    let body = request.body
+  // console.log("request headers", request.headers)
+  // console.log("request body", request.body)
+  let body = request.body
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    // if (body.name.length === 0 || body.number.length === 0) {
-    //     return response.status(400).json({
-    //         error: 'name and number are required fields'
-    //     })
-    // }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.get('/api/info/', (request, response) => {
 
-    Person.find({}).then(persons => {
-        // response.json(persons)
-        const personCount = persons.length
-        const dateObject = new Date();
-        // current date
-        // adjust 0 before single digit date
-        const date = (`0 ${dateObject.getDate()}`).slice(-2);
-        // current month
-        const month = (`0 ${dateObject.getMonth() + 1}`).slice(-2);
-        // current year
-        const year = dateObject.getFullYear();
-        // current hours
-        const hours = dateObject.getHours();
-        // current minutes
-        const minutes = dateObject.getMinutes();
-        // current seconds
-        const seconds = dateObject.getSeconds();
-        const ts = new Date(Date.now()).toLocaleString();
-        const tz = new Date().getTimezoneOffset()
-        response.send(`<p>Phonebook has info for ${personCount} people</p>
+  Person.find({}).then(persons => {
+    // response.json(persons)
+    const personCount = persons.length
+    // current date
+    // adjust 0 before single digit date
+    const ts = new Date(Date.now()).toLocaleString()
+    response.send(`<p>Phonebook has info for ${personCount} people</p>
             <p>${ts}</p>`)
-    })
+  })
 })
- 
-// Below the following add's middlewares 
 
+// Below the following add's middlewares
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 app.use(errorHandler)
--- backend/models/people.js
+++ backend/models/people.js
@@ -4,54 +4,56 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery',false)
 require('dotenv').config()
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
+  // eslint-disable-next-line no-unused-vars
   .then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 // Note on phone number validation:
 // The validation the number must in the format: `DDD-DDD-DDDD`
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: 3
-    },
-    number: {
-      type: String,
-      minLength: 8,
-      validate: {
-        validator: function(v) {
-          let isValid = false
-          // valid if all digits
-          isOnlyNumbers = /^[0-9]*$/.test(v)
-          if (isOnlyNumbers) return true
-          // console.log("only numbers check", isValid)
+  name: {
+    type: String,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        let isValid = false
+        // valid if all digits
+        let isOnlyNumbers = /^[0-9]*$/.test(v)
+        if (isOnlyNumbers) return true
+        // console.log("only numbers check", isValid)
 
-          // starts with two or three digits, followed by only digits
-          isValid = /^\d{2,3}-[0-9]*$/.test(v)
-          // console.log("hyphen check is valid", isValid)
-          
-          return isValid
-        }
-      },
-      required: [true, 'User phone number required']
-    }
+        // starts with two or three digits, followed by only digits
+        isValid = /^\d{2,3}-[0-9]*$/.test(v)
+        // console.log("hyphen check is valid", isValid)
+
+        return isValid
+      }
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
-const Person = mongoose.model('Person', personSchema)
+// const Person = mongoose.model('Person', personSchema)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
